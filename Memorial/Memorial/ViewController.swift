14729f00c9b4ebf7681978cecddd9fe35f56bea3
xi_jjun <rlawowns97@naver.com>
2022-09-17 21:59:09 +0900

    init Memorial project

---
+++ Memorial/Memorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memorial
+//
+//  Created by 김재준 on 2022/09/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
