581e908ad75dfb1e32e9eadb8d6a323e769067ff
xi_jjun <rlawowns97@naver.com>
2022-09-17 21:50:06 +0900

    feat: add Picture select view

--- MyFirstApp/MyFirstApp/PicturePickUpController.swift
+++ MyFirstApp/MyFirstApp/PicturePickUpController.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import UIKit
+import YPImagePicker
+
+class PicturePickUpController: UIViewController {
+    @IBOutlet weak var pictureImage: UIImageView!
+    @IBOutlet weak var pictureChangeBtn: UIButton!
+    
+    let imgPickerController = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("PicturePickUpController - init")
+        self.pictureImage.layer.cornerRadius = 5
+        self.pictureChangeBtn.layer.cornerRadius = 10
+        
+        pictureChangeBtn.addTarget(self, action: #selector(onPictureChangeBtnClicked), for: .touchUpInside)
+    }
+    
+    @objc fileprivate func onPictureChangeBtnClicked() {
+        print("PicturePickUpController - onPictureChangeBtnClicked called()")
+        
+        // camera configuration
+        var config = YPImagePickerConfiguration()
+        config.screens = [.library]
+        config.library.maxNumberOfItems = 3
+        
+        let picker = YPImagePicker(configuration: config)
+        picker.didFinishPicking { [unowned picker] items, _ in
+            if let photo = items.singlePhoto {
+                print(photo.fromCamera) // Image source (camera or library)
+                print(photo.image) // Final image selected by the user
+                print(photo.originalImage) // original image selected by the user, unfiltered
+                print(photo.modifiedImage) // Transformed image, can be nil
+                print(photo.exifMeta) // Print exif meta data of original image.
+                self.pictureImage.image = photo.image
+            }
+            picker.dismiss(animated: true, completion: nil)
+        }
+        present(picker, animated: true, completion: nil)
+    }
+}
