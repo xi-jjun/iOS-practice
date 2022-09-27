f11825a1bb9ca584cee40f754803cd253933c789
xi_jjun <rlawowns97@naver.com>
2022-09-27 18:29:18 +0900

    feat: create screen change app

---
+++ ScreenChangeApp/ScreenChangeApp/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ScreenChangeApp
+//
+//  Created by 김재준 on 2022/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func codeBtnTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func storyboardWithCodeBtnTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func storyboardWithSegueBtnTapped(_ sender: UIButton) {
+        
+    }
+    
+
+}
+
