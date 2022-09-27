eb94fd855ff6de2f7cfecf7f2cfa910982f58f08
xi_jjun <rlawowns97@naver.com>
2022-09-27 19:22:53 +0900

    feat: add FirstViewController for moving screen

--- ScreenChangeApp/ScreenChangeApp/FirstViewController.swift
+++ ScreenChangeApp/ScreenChangeApp/FirstViewController.swift
@@ -8,22 +8,67 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    let mainLabel: UILabel = {
+        let label = UILabel()
+        label.text = "FirstViewContoller"
+        label.font = .boldSystemFont(ofSize: 25)
+        
+        return label
+    }()
+    
+    let backBtn: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setTitle("BACK", for: .normal)
+        btn.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        btn.tintColor = .black
+        btn.addTarget(self, action: #selector(backBtnTapped), for: .touchUpInside)
+        
+        return btn
+    }()
+    
+    // optional이 아니면 기본값을 못주게 되어 FirstViewController에서 생성하기 까다롭다.
+    // 따라서 optional 설정을 하여 초기값을 nil로 설정한다.
+    var stringData: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        
+        
+        mainLabelAutoLayout()
+        backBtnAutoLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func mainLabelAutoLayout() {
+        view.addSubview(mainLabel)
+        mainLabel.text = stringData // ViewController에서 데이터를 가져와서 FisrtViewController의 mainLabel에 넣어주는 것
+        
+        mainLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mainLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
-    */
+    
+    func backBtnAutoLayout() {
+        view.addSubview(backBtn)
+        
+        backBtn.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            backBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            backBtn.widthAnchor.constraint(equalToConstant: 100),
+            backBtn.heightAnchor.constraint(equalToConstant: 40),
+            backBtn.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40)
+        ])
+    }
+    
+    @objc func backBtnTapped() {
+//        navigationController?.popViewController(animated: true)
+        dismiss(animated: true, completion: nil)
+    }
 
 }
--- ScreenChangeApp/ScreenChangeApp/ViewController.swift
+++ ScreenChangeApp/ScreenChangeApp/ViewController.swift
@@ -14,15 +14,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    
+    /**
+     code로 화면 이동 : 다음 화면이 코드로 작성되어 있을 때에만 가능한 방법
+     */
     @IBAction func codeBtnTapped(_ sender: UIButton) {
-        
+        let firstVC = FirstViewController() // instance 생성
+        firstVC.modalPresentationStyle = .fullScreen
+        firstVC.stringData = "Hello World!"
+        present(firstVC, animated: true, completion: nil)
     }
     
+    /**
+     code로 storyboard객체 생성하여 화면이동
+     */
     @IBAction func storyboardWithCodeBtnTapped(_ sender: UIButton) {
         
     }
     
+    /**
+     스토리보드에서의 화면이동(간접 세그웨이)
+     */
     @IBAction func storyboardWithSegueBtnTapped(_ sender: UIButton) {
         
     }
