9ac2d9c89784f3aac1df1f279a7a782b79ae7a5c
xi_jjun <rlawowns97@naver.com>
2023-02-12 18:18:32 +0900

    로컬 서버에 테스트 메세지 요청하는 api 추가

--- ApiRequestApp/ApiRequestApp/ViewController.swift
+++ ApiRequestApp/ApiRequestApp/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var requestBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor.black
     }
-
-
+    
+    @IBAction func requestBtnTapped(_ sender: UIButton) {
+        let requestUrl = "http://localhost:3000/test"
+        let structUrl = URL(string: requestUrl)!
+        
+        // dataTask
+        let dataTask = URLSession(configuration: .default).dataTask(with: structUrl) { (data: Data?, response: URLResponse?, error: Error?) in
+            // getting Data Error
+            guard error == nil else {
+                print("Error occur: \(String(describing: error))")
+                return
+            }
+            
+            guard let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                return
+            }
+            
+            // 통신에 성공한 경우 data에 Data 객체가 전달됩니다.
+            
+            // 받아오는 데이터가 json 형태일 경우,
+            // json을 serialize하여 json 데이터를 swift 데이터 타입으로 변환
+            // json serialize란 json 데이터를 String 형태로 변환하여 Swift에서 사용할 수 있도록 하는 것을 말합니다.
+            guard let jsonToArray = try? JSONSerialization.jsonObject(with: data, options: []) else {
+                print("json to Any Error")
+                return
+            }
+            // 원하는 작업
+            print(jsonToArray)
+        }.resume()
+    }
+    
 }
 
