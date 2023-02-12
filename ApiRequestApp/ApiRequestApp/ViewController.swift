77ddd662056bf847b3bc254c0779e1af332670c2
xi_jjun <rlawowns97@naver.com>
2023-02-12 17:26:41 +0900

    ios api request practice init

---
+++ ApiRequestApp/ApiRequestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiRequestApp
+//
+//  Created by 김재준 on 2023/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
